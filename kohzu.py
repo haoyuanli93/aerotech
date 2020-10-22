f1b6464c2a766adffd5553f3a625be61affa290d
Haoyuan Li <hyli16@stanford.edu>
2020-10-22 09:52:57 -0700

    new functions

---
+++ kohzu.py
@@ -0,0 +1,21 @@
+# Get the distance between the rotation center and
+# the edge of the crystal for T1
+def find_t1_rotation_center_from_edge(s1, d1, theta1, s2, d2, theta2, s3, d3, theta3):
+    """
+    Find the location of the center of rotation with respect to the tip of the splitting
+    edge of CC1
+    :param s1:
+    :param d1:
+    :param theta1:
+    :param s2:
+    :param d2:
+    :param theta2:
+    :param s3:
+    :param d3:
+    :param theta3:
+    :return:
+    """
+
+
+def move_t1_t6_together():
+    pass
